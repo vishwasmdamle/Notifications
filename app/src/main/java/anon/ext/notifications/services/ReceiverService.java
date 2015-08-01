35414619d80aa07b9cccd16413c5f1a37d191929
Vishwas Damle <vishwasmdamle@gmail.com>
2015-08-01 18:00:01 +0530

    Attaching notification listener.

--- app/src/main/java/anon/ext/notifications/services/ReceiverService.java
+++ app/src/main/java/anon/ext/notifications/services/ReceiverService.java
@@ -1,16 +1,43 @@
 package anon.ext.notifications.services;
 
-import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import android.util.Log;
 
-public class ReceiverService extends Service {
+public class ReceiverService extends NotificationListenerService {
+  private static boolean isAttached = false;
+  private final String TAG = getClass().getCanonicalName();
+
   public ReceiverService() {
   }
 
   @Override
   public IBinder onBind(Intent intent) {
-    // TODO: Return the communication channel to the service.
-    throw new UnsupportedOperationException("Not yet implemented");
+    Log.e(TAG, "Notification Listener Service Attached");
+    isAttached = true;
+    return super.onBind(intent);
+  }
+
+  public static boolean isAttached() {
+    return isAttached;
+  }
+
+  @Override
+  public void onNotificationPosted(StatusBarNotification sbn) {
+    super.onNotificationPosted(sbn);
+  }
+
+  @Override
+  public void onNotificationRemoved(StatusBarNotification sbn) {
+    super.onNotificationRemoved(sbn);
+  }
+
+  @Override
+  public boolean onUnbind(Intent intent) {
+    Log.e(TAG, "Notification Listener Service Detached");
+    isAttached = false;
+    return super.onUnbind(intent);
   }
 }
