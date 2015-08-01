9570568a46e4e38aae9c3f26dafce3b9c36c7af9
Vishwas Damle <vishwasmdamle@gmail.com>
2015-08-01 16:25:30 +0530

    First commit

---
+++ app/src/main/java/anon/ext/notifications/activities/NotificationActivity.java
@@ -0,0 +1,16 @@
+package anon.ext.notifications.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import anon.ext.notifications.R;
+
+
+public class NotificationActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_notification);
+  }
+}
